ab1712a84963899194629fe3c488b46b86ecf6fc
NguyenAnhDuc1711 <ducna17112003@gmail.com>
2024-10-24 15:37:52 +0700

    fix-cf

--- fe/src/pages/ChatPage.jsx
+++ fe/src/pages/ChatPage.jsx
@@ -3,55 +3,6 @@ import LeftSideBarMsg from "../components/Message/LeftSideBar";
 import RightSideMsg from "../components/Message/RightSide";
 
 const ChatPage = () => {
-  const bgColor = useColorModeValue("cbg.light", "cbg.dark");
-  const userInfo = useSelector((state) => state.user.userInfo);
-  const [users, setUsers] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  const init = useRef(true);
-  const [showRightBar, setShowRightBar] = useState(false);
-  const [selectedUser, setSelectedUser] = useState(null);
-
-  useEffect(() => {
-    if (!init.current && searchValue) {
-      handleGetUsers({
-        page: 1,
-        searchValue,
-        isFetchMore: false,
-      });
-    }
-    init.current = false;
-  }, [searchValue]);
-
-  const handleGetUsers = async ({
-    page,
-    searchValue,
-    isFetchMore,
-    setHasMore = null,
-  }) => {
-    try {
-      const data = await GET({
-        path: Route.USER + USER_PATH.USERS_TO_FOLLOW,
-        params: {
-          userId: userInfo._id,
-          page: page,
-          limit: 8,
-          searchValue,
-        },
-      });
-      if (data.length > 0) {
-        if (isFetchMore) {
-          setUsers((prevUsers) => [...prevUsers, ...data]);
-        } else {
-          setUsers(data);
-        }
-      } else {
-        // setHasMore && setHasMore(false);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
   return (
     <Flex
       position={"absolute"}
